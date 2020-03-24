261f6677ab2592727ff90f9f67103ef7731e9087
Mateus Targino <mateusntargino@gmail.com>
2020-03-24 01:16:47 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var button = document.getElementById("icon")
+var state = document.getElementById("state")
+var mama = document.getElementById("mama")
+var text = ["&nbsp Má", "Boa"]
+
+function change() {
+    button.classList.toggle("icon-good");
+    state.classList.toggle("good");
+    if(mama.innerHTML == text[1]) {
+        mama.innerHTML = text[0];    
+    }
+    else {
+        mama.innerHTML = text[1];
+    }
+    
+}
+
+
+
+
